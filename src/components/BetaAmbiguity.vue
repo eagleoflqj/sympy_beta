1b934c117aa136771a0ed3716ed62d664708cb92
eagleoflqj <liumeo@pku.edu.cn>
2022-01-08 17:52:50 -0500

    fix did you mean style

--- src/components/BetaAmbiguity.vue
+++ src/components/BetaAmbiguity.vue
@@ -14,35 +14,26 @@ defineProps({
 </script>
 
 <template>
-  <n-card class="did_you_mean">
+  <n-card
+    class="result_card"
+    style="text-align: center;
+  box-shadow: 0px 0px 5px 2px rgba(255, 127, 14, 0.5)"
+  >
     Did you mean:
     <router-link :to="{ name: 'Result', params: { expr: ambiguity } }">
       {{ ambiguity }}
     </router-link>?
     <br>
     <template v-for="item in description">
-      <template v-if="item.type === 'Expression'">
-        <router-link :to="{ name: 'Result', params: { expr: item.value } }">
-          {{ item.value }}
-        </router-link>
-      </template>
+      <router-link
+        v-if="item.type === 'Expression'"
+        :to="{ name: 'Result', params: { expr: item.value } }"
+      >
+        {{ item.value }}
+      </router-link>
       <template v-else-if="item.type === 'Text'">
         {{ item.value }}
       </template>
     </template>
   </n-card>
 </template>
-
-<style>
-.did_you_mean {
-  text-align: center;
-  box-shadow: 0px 0px 5px 2px rgba(255, 127, 14, 0.5);
-}
-
-@media screen and (max-device-width: 767px) {
-  .did_you_mean {
-    box-shadow: none;
-    border: 1px solid #f33;
-  }
-}
-</style>
