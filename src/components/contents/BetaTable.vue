eb9a699eda90df3259db327cf38930ed8c66e432
Qijia Liu <liumeo@pku.edu.cn>
2022-03-13 19:23:23 -0400

    SBUI: limit table number of rows to 10

--- src/components/contents/BetaTable.vue
+++ src/components/contents/BetaTable.vue
@@ -1,36 +1,51 @@
 <script setup>
-import { NTable } from 'naive-ui'
+import { NTable, NScrollbar } from 'naive-ui'
 import BetaContainer from '@/components/BetaContainer.vue'
+import { onMounted } from 'vue'
 
-defineProps({
+const props = defineProps({
   card: {
     type: Object,
     default: () => {}
   }
 })
+
+const maxNRow = 10
+onMounted(() => {
+  const { card } = props
+  const nColumn = Math.ceil(card.rows.length / maxNRow)
+  card.titles.splice(0, card.titles.length, ...[].concat(...new Array(nColumn).fill(card.titles)))
+  const remainingRows = card.rows.splice(maxNRow)
+  for (const i in remainingRows) {
+    const j = i % maxNRow
+    card.rows[j].splice(card.rows[j].length, 0, ...remainingRows[i])
+  }
+})
 </script>
 
 <template>
-  <n-table style="width: 0; margin: auto">
-    <thead>
-      <tr>
-        <th v-for="title in card.titles">
-          {{ title }}
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="row in card.rows">
-        <td v-for="cell in row">
-          <beta-container
-            v-if="typeof cell === 'object'"
-            :card="cell"
-          />
-          <template v-else>
-            {{ cell }}
-          </template>
-        </td>
-      </tr>
-    </tbody>
-  </n-table>
+  <n-scrollbar x-scrollable>
+    <n-table style="width: 0; margin: auto">
+      <thead>
+        <tr>
+          <th v-for="title in card.titles">
+            {{ title }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in card.rows">
+          <td v-for="cell in row">
+            <beta-container
+              v-if="typeof cell === 'object'"
+              :card="cell"
+            />
+            <template v-else>
+              {{ cell }}
+            </template>
+          </td>
+        </tr>
+      </tbody>
+    </n-table>
+  </n-scrollbar>
 </template>
