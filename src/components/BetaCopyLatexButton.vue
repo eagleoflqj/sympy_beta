3e50e3fe2d36ef9d0d37bef683c49ac2c62b8194
Qijia Liu <liumeo@pku.edu.cn>
2022-03-15 20:39:31 -0400

    SBUI: copy latex

---
+++ src/components/BetaCopyLatexButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Math } from '@vicons/tabler'
+import BetaCopyButton from '@/components/BetaCopyButton.vue'
+
+defineProps({
+  tex: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <beta-copy-button
+    :copied="tex"
+    :icon-component="Math"
+    popover-text="Copy LaTex"
+  />
+</template>
--- src/components/contents/BetaTex.vue
+++ src/components/contents/BetaTex.vue
@@ -1,18 +1,27 @@
 <script setup>
+import { computed } from 'vue'
 import { NScrollbar } from 'naive-ui'
+import BetaCopyLatexButton from '@/components/BetaCopyLatexButton.vue'
 
-defineProps({
+const props = defineProps({
   card: {
     type: Object,
-    default: () => {}
+    required: true
   }
+})
+
+const tex = computed(() => {
+  return props.card.tex + (props.card.numeric ? '\\approx' + props.card.approximation : '')
 })
 </script>
 
 <template>
+  <div style="text-align: right">
+    <beta-copy-latex-button :tex="tex" />
+  </div>
   <n-scrollbar x-scrollable>
     <vue-mathjax
-      :formula="'$$' + card.tex + (card.numeric ? '\\approx' + card.approximation : '') + '$$'"
+      :formula="'$$' + tex + '$$'"
     />
   </n-scrollbar>
 </template>
